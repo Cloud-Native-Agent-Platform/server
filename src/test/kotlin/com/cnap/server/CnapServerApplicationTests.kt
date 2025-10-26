65e7f26baead78d907aa1cb3ce00feae23072c0f
dwl21 <nggus5@gmail.com>
2025-10-27 01:07:07 +0900

    [#3]; chore: bootstrap Spring Boot project with core configurations and HealthController

---
+++ src/test/kotlin/com/cnap/server/CnapServerApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.cnap.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CnapServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
