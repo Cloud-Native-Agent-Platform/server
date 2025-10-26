a1c2fc680bbc9b66702f9be363307aa2d0933f7d
dwl21 <nggus5@gmail.com>
2025-10-27 01:22:21 +0900

    chore: add JPA auditing configuration and BaseEntity for audit fields

---
+++ src/main/kotlin/com/cnap/server/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package com.cnap.server.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig
